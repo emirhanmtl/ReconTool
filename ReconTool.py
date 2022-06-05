fc3d55ceb252c5dfeab3e2d794bf2376834768d6
Emirhan Mutlu <emirhanmutlu35@gmail.com>
2022-06-05 17:18:37 +0300

    few changes

--- ReconTool.py
+++ ReconTool.py
@@ -35,7 +35,7 @@ print(banner)
 
 input_domain = input("Please enter the domain (Example = tesla.com): ")
 
-command =  "curl -s https://crt.sh/\?q\=%25.{domain}\&output\=json |jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | httprobe -c 50 | aquatone"
+command =  "curl -s https://crt.sh/\?q\=%25.{domain}\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | httpx -status-code -o subs.txt"
 
 
 os.system(command.format(domain = input_domain))
